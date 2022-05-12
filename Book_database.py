39e789d0c68e7e0e3a71da9a6072c200ef3b727d
TheOneChris <60333802+TheOneChris@users.noreply.github.com>
2022-05-12 15:30:38 -0400

    Final finished

---
+++ Book_database.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+
+def main():
+    with sqlite3.connect("database.db") as d:
+        c = d.cursor()
+        c.execute("""
+        CREATE TABLE IF NOT EXISTS books(
+        ISBN INTEGER PRIMARY KEY,
+        Title TEXT,
+        Authors TEXT,
+        PageCount TEXT,
+        AverageRating TEXT,
+        Thumbnail TEXT
+        )
+        """)
+
+    d.commit()
+
+
+if __name__ == '__main__':
+    main()
